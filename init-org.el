bac18aeca98728fc6a5bd5b973ce2c96a3c9e48f
Mathias Tausen <mathias.tausen@gmail.com>
2015-03-06 22:51:53 +0100

    Dont load epresent

--- init-org.el
+++ init-org.el
@@ -42,8 +42,8 @@
 (global-set-key (kbd "C-c o a") 'org-agenda)
 
 ;; presentations via org-mode
-(add-to-list 'load-path "~/.emacs.d/lib/epresent")
-(load "~/.emacs.d/lib/epresent/epresent.el")
+;; (add-to-list 'load-path "~/.emacs.d/lib/epresent")
+;; (load "~/.emacs.d/lib/epresent/epresent.el")
 
 ;; drag n drop image into to org-mode buffer
 (add-to-list 'load-path "~/.emacs.d/lib/org-download")
